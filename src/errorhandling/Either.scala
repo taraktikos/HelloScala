cb69947c39bb295c23a49efcc00a836f00744eac
tsuhovenko <tsuhovenko@cogniance.com>
2015-05-21 15:59:59 +0300

    either

--- src/errorhandling/Either.scala
+++ src/errorhandling/Either.scala
@@ -1,9 +1,59 @@
 package errorhandling
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(v) => Right(f(v))
+    case Left(e) => Left(e)
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(v) => f(v)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      b1 <- b
+    } yield f(a, b1)
+}
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 object Either {
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty) Left("mean of empty list")
+    else Right(xs.sum / xs.length)
 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+
+  def insuranceRateQuote(age: Int, number: Int): Int = {
+    print(age, number)
+    1
+  }
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeed: String): Either[Exception, Double] =
+    for {
+      a <- Try { age.toInt }
+      tickets <- Try { numberOfSpeed.toInt }
+    } yield insuranceRateQuote(a, tickets)
+
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match {
+    case Nil => Right(Nil)
+    case h :: t => (f(h) map2 traverse(t)(f))(_ :: _)
+  }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(x => x)
 }
